c83a61e02a359d3cbf339cd2abd1f1147c99c1cc
u0052041 <u0052041@gmail.com>
2021-01-30 19:01:39 +0800

    Add product api SaleTop3, code format

--- product/views.py
+++ product/views.py
@@ -1,8 +1,14 @@
+from django.db.models import Count, Sum
+from django.db.models.functions import Coalesce
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from order.models import Order
+
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -12,6 +18,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "id"
+
+    @action(methods=["GET"], detail=False, url_path="top3")
+    def sales_top3(self, request):
+        """
+            calc order qty sum in different product
+        """
+        proudct_top3 = (
+            Product.objects.all()
+            .annotate(sales_sum=Coalesce(Sum("order__qty"), 0))
+            .order_by("-sales_sum")[:3]
+        )
+        response_json = {"top3_product": list(proudct_top3.values("id", "sales_sum"))}
+        return Response(response_json, status=status.HTTP_200_OK)
 
 
 def datatable_product_list(request):
@@ -23,9 +42,11 @@ def datatable_product_list(request):
 
     product = Product.objects.all().order_by("id")
     select_product = product[start : start + length]
-    
+
     json_response = dict()
-    json_response["data"] = list(select_product.values("id", "stock_pcs", "price", "shop__id", "vip_only"))
+    json_response["data"] = list(
+        select_product.values("id", "stock_pcs", "price", "shop__id", "vip_only")
+    )
     json_response["draw"] = draw
     json_response["length"] = length
     json_response["recordsFiltered"] = product.count()
